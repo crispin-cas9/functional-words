88b2a18ffd18e6576ceff983a4cac68cc3645471
Crispin <starkwafie@gmail.com>
2016-12-08 09:37:20 -0800

    backup

--- functional_words.py
+++ functional_words.py
@@ -5,11 +5,7 @@ import urllib2
 from pprint import pprint
 import itertools
 
-# fword = functional word
-
 fwords = urllib2.urlopen("https://fling.seas.upenn.edu/~maeisen/wiki/functionwords.txt").read().split(" ")
-
-# shakespeare = urllib2.urlopen("http://cs.stanford.edu/people/karpathy/char-rnn/shakespeare_input.txt").read().lower()
 
 rawdata = open("tempest.txt").read().lower()
 data = re.findall(r"[^.;:!?]+", rawdata)
@@ -30,19 +26,7 @@ for sentence in data:
 		
 	indices.append(sdict)
 
-# print indices
-
 diffdict = {}
-
-# loop over every sentence
-# loop over the words in those sentences
-# the first word should be the key for the first dictionary
-# the value for that is -- a new dictionary yay!
-# the first key in dict2 is the second word in the sentence
-# the second key is the third word, etc
-# the value for each key is the key word minus the dict word
-
-# this function returns the difference between two words:
 
 def getdistance(list1, list2):
 	pairs = itertools.product(list1, list2)
@@ -64,32 +48,3 @@ for sentence in indices:
 			diffdict[word1] = wordict
 
 pprint(diffdict)
-
-
-# 			if diffdict.has_key(word1):
-# 				if diffdict[word1].has_key(word2):
-# 					total = sum(diffdict[word1][word2])
-# 					average = float(total) / len(diffdict[word1][word2])
-# 					diffdict[word1][word2] = average
-# 				else:
-# 					diffdict[word1][word2] = wordict[word2]
-# 			else:
-# 				diffdict[word1] = wordict
-
-# for sentence in indices:
-# 	words = sentence.keys()
-# 	for index, word1 in enumerate(words):
-# 		wordict = {}
-# 		total = 0
-# 		count = 0
-# 		for word2 in words[index+1:]:
-# 			wordict[word2] = abs(sentence[word2] - sentence[word1])
-# 			total = total + wordict[word2]
-# 			count = count + 1
-# 			if diffdict.has_key(word1):
-# 				if diffdict[word1].has_key(word2):
-# 					diffdict[word1][word2] = float(total) / count
-# 				else:
-# 					diffdict[word1][word2] = wordict[word2]
-# 			else:
-# 				diffdict[word1] = wordict
